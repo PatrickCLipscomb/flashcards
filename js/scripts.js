465871f21346133972128da4251c299a8d2c0bec
Kevin VanEvery and Patrick Lipscomb <patrick@>
2016-05-31 13:11:29 -0700

    created two flashcards that toggle definition on click

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function () {
+  $("a#operators").click(function() {
+    $("p#operators").toggle();
+  });
+});
+
+$(function () {
+  $("a#javascript").click(function() {
+    $("p#javascript").toggle();
+  });
+});
