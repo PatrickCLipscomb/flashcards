447ae53ed19fe36565b279366fbb5c1ff796897c
Kevin VanEvery and Patrick Lipscomb <patrick@>
2016-05-31 15:23:16 -0700

    solution for independent flascard buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
-$(function () {
-  $("a#operators").click(function() {
-    $("p#operators").toggle();
-  });
-});
+// $(function () {
+//   $("a#operators").click(function() {
+//     $("p#operators").toggle();
+//   });
+// });
 
 $(function () {
-  $("a#javascript").click(function() {
-    $("p#javascript").toggle();
+  $("a.reveal").click(function() {
+    // debugger;
+    var cardName = $(this).attr("name");
+    console.log(cardName);
+    $('#' + cardName).toggle();
+        // $('p.definition').toggle();
   });
 });
